c5fbd2da1e0f92fb4bb266409c122d111ec2b27e
Dermo909 <80477146+Dermo909@users.noreply.github.com>
2021-04-18 16:31:02 +0100

    added API documentation

--- node_lab1/index.js
+++ node_lab1/index.js
@@ -1,15 +1,20 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'js-yaml';
+import fs from 'fs';
 
 dotenv.config();
 
+const swaggerDocument = yaml.load(fs.readFileSync('./../movie-api-yaml/swagger.yaml', 'utf8'));
+console.log(swaggerDocument);
 const app = express();
 
 const port = 8080; //process.env.PORT;
 
 app.use(express.json());
-
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/movies', moviesRouter);
 
 app.listen(port, () => {
