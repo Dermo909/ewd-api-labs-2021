3510f90d8d846d33a371c10297fa738acaa988c7
Dermo909 <80477146+Dermo909@users.noreply.github.com>
2021-04-18 16:11:17 +0100

    Initial express setup

--- node_lab1/index.js
+++ node_lab1/index.js
@@ -1,17 +1,15 @@
-// Load the http module to create an http server.
-import http from 'http';
 import dotenv from 'dotenv';
+import express from 'express';
+import moviesRouter from './api/movies';
 
 dotenv.config();
 
-const port = 8080; // process.env.PORT;
-// Configure our HTTP server to respond with Hello World to all requests.
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello EWD! ');
+const app = express();
+
+const port = 8080; //process.env.PORT;
+
+app.use('/api/movies', moviesRouter);
+
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
 });
-
-server.listen(port);
-
-// Put a friendly message on the terminal
-console.log(`Server running at ${port}`);
